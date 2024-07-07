ff206e9a531d4fd9f35f1d1699c748a7aefaade9
Remmy4873 <Remmykiplangat4873@gmail.com>
2024-07-07 15:54:38 +0300

    array work

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+
+function superbowlWin(record) {
+    
+    let winningEntry = record.find(entry => entry.result === "W");
+    return winningEntry ? winningEntry.year : undefined;
+}
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+];
+
+console.log(superbowlWin(record)); 
